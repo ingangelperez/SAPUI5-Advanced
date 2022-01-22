3c918eda3c6138b919f103adf22962ce4d18e262
ingangelperez <ing.perez.angel@gmail.com>
2022-01-22 22:00:56 +0000

    Actividad Bloque 5.2

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -10,6 +10,23 @@ sap.ui.define([
         return Controller.extend("aapg.employees.controller.MainView", {
             onInit: function () {
 
+            },
+
+            onValidateEmployeeInput: function (oEvent) {
+
+                var oInputEmployee = oEvent.getSource();
+                var oLabelCountry = this.getView().byId("labelSelectCountry");
+                var oSelectCountry = this.getView().byId("slCountry");
+
+                if (oInputEmployee.getValue().length === 6) {
+                    oLabelCountry.setVisible(true);
+                    oSelectCountry.setVisible(true);
+                } else {
+                    oLabelCountry.setVisible(false);
+                    oSelectCountry.setVisible(false);
+                }
+
             }
+
         });
     });
